5457c32dc947853b0fad4680dbc12af5def6e565
0xdagarn <sgnoo@naver.com>
2025-05-31 15:51:48 +0900

    feat: set json_api routes

--- lib/offtherecord/record.ex
+++ lib/offtherecord/record.ex
@@ -1,6 +1,18 @@
 defmodule Offtherecord.Record do
   use Ash.Domain, extensions: [AshJsonApi.Domain]
 
+  json_api do
+    routes do
+      base_route "/posts", Offtherecord.Record.Post do
+        get :read
+        index :read
+        post :create
+        patch :update, route: "/:id"
+        delete :destroy, route: "/:id"
+      end
+    end
+  end
+
   resources do
     resource Offtherecord.Record.Post
   end
